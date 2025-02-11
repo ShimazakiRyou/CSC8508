c000e32c6f2039ef3a02463d00375ad2c1c28787
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2025-02-11 17:13:35 +0000

    Buffers are Cool

--- CSC8508CoreClasses/ComponentManager.h
+++ CSC8508CoreClasses/ComponentManager.h
@@ -1,17 +1,17 @@
-//
-// Contributors: Alasdair
-//
-
 #ifndef COMPONENTMANAGER_H
 #define COMPONENTMANAGER_H
+
 #include <typeindex>
 #include <iostream>
 #include <fstream>
-using std::vector;
 #include <vector>
+#include <unordered_map>
+#include <memory>
+#include <type_traits>
 
-namespace NCL::CSC8508 
-{
+using std::vector;
+
+namespace NCL::CSC8508 {
 
     class IComponent;
 
@@ -24,7 +24,7 @@ namespace NCL::CSC8508
             auto it = allComponents.find(typeid(T));
             if (it != allComponents.end()) {
                 for (auto* comp : it->second) {
-                    if (auto* derived = dynamic_cast<T*>(comp)) 
+                    if (auto* derived = dynamic_cast<T*>(comp))
                         result.push_back(derived);
                 }
             }
@@ -34,12 +34,18 @@ namespace NCL::CSC8508
         template <typename T, typename... Args>
             requires std::is_base_of_v<IComponent, T>
         static T* AddComponent(Args&&... args) {
-            T* component = new T(std::forward<Args>(args)...);
-            allComponents[typeid(T)].push_back(component);            
+            if (componentCount<T> >= MAX_COMPONENTS<T>) {
+                std::cerr << "The Component pool is filled:: Increase the max size or reduce component Count" << std::endl;
+                return nullptr;
+            }
+            T* component = new (componentBuffer<T> +componentCount<T> *sizeof(T)) T(std::forward<Args>(args)...);
+            componentCount<T>++;
+            allComponents[typeid(T)].push_back(component);
             return component;
         }
 
-        static void Clear() {
+        static void Clear() 
+        {
             for (auto& [type, componentsList] : allComponents) {
                 for (auto* comp : componentsList) 
                     delete comp;
@@ -50,13 +56,25 @@ namespace NCL::CSC8508
     private:
         ComponentManager() = default;
 
-        template <typename T> requires std::is_base_of_v<IComponent,T>static vector<T*> components;
+        template <typename T> requires std::is_base_of_v<IComponent, T>
+        static constexpr size_t MAX_COMPONENTS = 1000;
+
+        template <typename T> requires std::is_base_of_v<IComponent, T>
+        static size_t componentCount;
+
+        template <typename T> requires std::is_base_of_v<IComponent, T>
+        static alignas(T) char componentBuffer[MAX_COMPONENTS<T> *sizeof(T)];
         inline static std::unordered_map<std::type_index, std::vector<IComponent*>> allComponents;
     };
 
     template <typename T>
         requires std::is_base_of_v<IComponent, T>
-    vector<T*> ComponentManager::components = vector<T*>();
+    size_t ComponentManager::componentCount<T> = 0;
+
+    template <typename T>
+        requires std::is_base_of_v<IComponent, T>
+    char ComponentManager::componentBuffer<T>[MAX_COMPONENTS<T> *sizeof(T)] = {};
+
 }
 
-#endif //COMPONENTMANAGER_H
+#endif // COMPONENTMANAGER_H
